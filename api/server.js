713efbdf480213c55a052626de3982cb9b9f5e7c
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-10-21 16:40:11 -0500

    edited routes

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const helmet = require('helmet')
 const cors = require('cors')
+const restricted = require('./auth/utils/restricted-middleware')
 
 //route imports
 const usersRouter = require('./auth/users/users-router')
@@ -11,15 +12,17 @@ const campaignRouter = require('./campaign/campaign-router')
 //initiate server
 const server = express()
 
-
 //global middleware
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
-//routes
+// user routes
 server.use('/api', usersRouter)
-// server.use('/api', campaignRouter)
+
+//auth routes
+server.use(restricted)
+server.use('/api', campaignRouter)
 
 
 module.exports = server
