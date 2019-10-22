7676632b7353dadfe7f4ed06ae24811b97ec8d5a
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-10-22 12:54:21 -0500

    finished endpoints

--- api/campaign/campaign-router.js
+++ api/campaign/campaign-router.js
@@ -1,54 +1,71 @@
-const express = require('express')
-const campaignModel = require('./campagin-model')
+const express = require('express');
+const campaignModel = require('./campagin-model');
 
 
 //middleware
-const validateCampaign = require('./utils/validate-campaign')
-const getPrediction = require('./utils/getPrediction-middleware')
+const validateFields = require('./utils/validate-campaign-fields');
+const getPrediction = require('./utils/getPrediction-middleware');
+const validateId = require('./utils/validate-campaign-id')
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/campaigns', validateCampaign, getPrediction, async (req, res) => {
-	let newCampaign = req.body
-	const id = req.userid
-	newCampaign = { user_id: id, ...newCampaign, }
+router.post('/campaigns', validateFields, getPrediction, async (req, res) => {
+	let newCampaign = req.body;
+	const id = req.userid;
+	newCampaign = { user_id: id, ...newCampaign, };
 
 	const addedCampaign = await campaignModel.add(newCampaign)
 		try {
-			res.status(201).json(addedCampaign)
+			res.status(201).json(addedCampaign);
 		} 
+
 		catch (error) {
-			res.status(500).json({ errMessage: "internal database error", error })
+			res.status(500).json({ errMessage: "internal database error", error });
 		}
-})
+});
 
 router.get('/campaigns', (req, res) => {
 	const user_id = req.userid
 	campaignModel.getBy({ user_id })
 		.then(campaigns => {
-				res.status(200).json(campaigns)
+				res.status(200).json(campaigns);
 		})
+
 		.catch(err => {
-			res.status(500).json({ errMessage: "internal database error", err })
+			res.status(500).json({ errMessage: "internal database error", err });
 		})
-})
+});
 
-router.put('/campaigns/:id', validateCampaign, getPrediction, (req, res) => {
-	const updatedCampaign = req.body
+router.put('/campaigns/:id', validateId, validateFields, getPrediction, (req, res) => {
+	const updatedCampaign = req.body;
+	const id = req.params.id;
 
-	 campaignModel.update(req.params.id, updatedCampaign) 
-	 	.then(campaign => {
-			res.status(200).json(campaign)
+	 campaignModel.update(id, updatedCampaign)
+	 	.then(dbRes => {
+			campaignModel.getBy( {id} ).first()
+				.then(campaign => {
+					res.status(200).json(campaign);
+				})
+
+				.catch(err => {
+					res.status(500).json(err)
+				})
 		 })
 
 		.catch(err => {
-		res.status(500).json({ errMessage: "internal database error could not update campaign", err })
+		res.status(500).json({ errMessage: "internal database error, could not update campaign", err });
+		})
+});
+
+router.delete('/campaigns/:id', validateId, (req, res) => {
+	
+	campaignModel.remove(req.params.id)
+		.then(campaign => {
+			res.status(200).json(campaign)
 		})
 
-
-})
-
-router.delete('/campaigns/id:', (req, res) => {
-
-})
-module.exports = router
+		.catch(err => {
+			res.status(500).json({errMessage: "internal database error, could not update campaign", err })
+		})
+});
+module.exports = router;
