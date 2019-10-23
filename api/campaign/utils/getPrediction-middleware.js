17b94675dcb41d2261c19e19b228f4f5c0095bb4
MSquared88 <39702456+MSquared88@users.noreply.github.com>
2019-10-23 15:43:51 -0500

    changed DS url and added test files

--- api/campaign/utils/getPrediction-middleware.js
+++ api/campaign/utils/getPrediction-middleware.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 
 module.exports = (req, res, next) => {
 	let newCampaign = req.body
-	axios.post('http://bc5322e4.ngrok.io/predict', newCampaign)
+	axios.post('http://196d8cec.ngrok.io/predict', newCampaign)
 		.then(res => {
 			req.body.success = res.data.pred
 			next()
